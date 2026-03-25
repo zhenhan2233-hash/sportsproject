b8a0b2cd0f0e76c89d7ae928f09e89d18335a223
zhenhan2233-hash <zhenhan2233@gmail.com>
2026-03-25 10:39:19 -0700

    few changes

--- src/components/Prof.jsx
+++ src/components/Prof.jsx
@@ -1,21 +1,8 @@
-function Prof({ Name, Sport, Info, Events}) {
+function Prof({ Name, Sport, Info, Events, Pic }) {
   return (
-    <div className="bg-white rounded-2xl shadow-md p-6 w-72 text-center">
-      <h2 className="text-bold">User:{Name} </h2>
-
-      <h3 className = "text-bold text-blue-800">Sport Interests:{Sport}</h3>
-
-      <h3 className = "text-blue-800">Additional Information: {Info}</h3>
-
-      <h4 className = "text-bold text-gray-800">Events: {Events}</h4>
-      </div>
-
-
-      
-
-
+    <h1 className>User: {Name} </h1>
     
   );
 }
 
-export default Prof;
+export default Food;
