cd1872dd8e0eea38e58d908655cd6a10050977af
Ray191u4182481284 <s_raymon.chen@ousd.org>
2026-04-13 11:31:08 -0700

    h

--- src/components/Prof.jsx
+++ src/components/Prof.jsx
@@ -1,41 +1,8 @@
-import React, { useState } from "react";
-
-function Prof() {
-  const [name, setName] = useState("");
-  const [sport, setSport] = useState("");
-  const [info, setInfo] = useState("");
-  const [events, setEvents] = useState("");
-  const [saved, setSaved] = useState(false);
-
-  function handleSubmit(e) {
-    e.preventDefault();
-    setSaved(true);
-  }
-
+function Prof({ Name, Sport, Info, Events, Pic }) {
   return (
-    <div className="flex flex-col items-center gap-6 mt-10 mb-24">
-
-      <form onSubmit={handleSubmit} className="flex flex-col gap-3 w-72">
-        <input placeholder="Name" onChange={(e) => setName(e.target.value)} className="border p-2 rounded"/>
-        <input placeholder="Sport" onChange={(e) => setSport(e.target.value)} className="border p-2 rounded"/>
-        <input placeholder="Info" onChange={(e) => setInfo(e.target.value)} className="border p-2 rounded"/>
-        <input placeholder="Events" onChange={(e) => setEvents(e.target.value)} className="border p-2 rounded"/>
-
-        <button className="bg-blue-500 text-white p-2 rounded">
-          Save
-        </button>
-      </form>
-
-      {saved && (
-        <div className="bg-white rounded-2xl shadow-md p-6 w-72 text-center">
-          <h2 className="font-bold">User: {name}</h2>
-          <h3 className="font-bold text-blue-800">Sport Interests: {sport}</h3>
-          <h3 className="text-blue-800">Additional Information: {info}</h3>
-          <h4 className="font-bold text-gray-800">Events: {events}</h4>
-        </div>
-      )}
-    </div>
+    <h1>hi</h1>
+    
   );
 }
 
-export default Prof;
+export default Food;
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,29 +1,7 @@
-import pic1 from "../assets/IMG_2951.JPG";
-import pic2 from "../assets/IMG_4446.JPG";
-
-
-import Card from "../components/Card";
-
 function About() {
-  return (
-    <div style={{ display: "flex", gap: "20px" }}>
-      <Card 
-        name="Anthony"
-        background="Student at Oakland Technical High School"
-        goals="I want to help users that are have inadequate access to sporting facilities and events."
-        skills="Experienced in sports | Avid Sports Watcher"
-        pic={pic1}
-      />
+  return <h1></h1>;
 
-      <Card 
-        name="Raymon"
-        background="Student at Oakland Technical High School"
-        goals="Providing a great experience for people in need of Sports Assistance."
-        skills="Kind of Experienced in sports | Avid Sports Watcher and Betting Loser"
-        pic={pic2}
-      />
-    </div>
-  );
+  
 }
 
 export default About;
