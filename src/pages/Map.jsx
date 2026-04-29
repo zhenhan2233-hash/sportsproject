792a0621f74615d04cf9ee9794c2481fd551fd41
Ray191u4182481284 <s_raymon.chen@ousd.org>
2026-04-29 10:39:36 -0700

    a

--- src/pages/Map.jsx
+++ src/pages/Map.jsx
@@ -1,15 +1,21 @@
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
+import "leaflet-geosearch/dist/geosearch.css";
+
+import MapSearch from "../components/MapSearch"; // 👈 ADD THIS
 
 function Map() {
   return (
-    <div style={{ height: "100vh", width: "100%", paddingBottom: "120px" }}>
+    <div style={{ height: "100vh", width: "100%" }}>
       <MapContainer
         center={[37.7749, -122.4194]}
         zoom={13}
         style={{ height: "100%", width: "100%" }}
       >
         <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+
+        {/* 👇 ADD SEARCH BAR */}
+        <MapSearch />
 
         <Marker position={[37.7749, -122.4194]}>
           <Popup>My Location</Popup>
